6dd3a1632cbe3150573f003b80d2607313cdd390
rakuishi <rakuishi@gmail.com>
2017-03-26 10:39:18 +0900

    Use backStack for control of fragments

--- app/src/main/java/com/rakuishi/postalcode/view/activity/MainActivity.java
+++ app/src/main/java/com/rakuishi/postalcode/view/activity/MainActivity.java
@@ -1,6 +1,8 @@
 package com.rakuishi.postalcode.view.activity;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 
@@ -9,6 +11,9 @@ import com.rakuishi.postalcode.view.fragment.PostalCodeListFragment;
 
 public class MainActivity extends BaseActivity {
 
+    // FIXME: I want to use `FragmentManager.getBackStackEntryCount()` for control of fragments.
+    private int count = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,12 +21,47 @@ public class MainActivity extends BaseActivity {
         appComponent().inject(this);
 
         if (savedInstanceState == null) {
-            PostalCodeListFragment fragment = new PostalCodeListFragment();
-            FragmentManager manager = getSupportFragmentManager();
-            FragmentTransaction transaction = manager.beginTransaction();
-            transaction.add(R.id.container, fragment);
-            // transaction.addToBackStack(fragment.getClass().getSimpleName());
-            transaction.commit();
+            PostalCodeListFragment fragment = PostalCodeListFragment.newInstance(PostalCodeListFragment.Type.PREFECTURE);
+            replaceFragment(fragment);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        FragmentManager manager = getSupportFragmentManager();
+        if (count > 1) {
+            manager.popBackStack();
+            count--;
+            updateShowHomeButton();
+            return;
+        }
+
+        super.onBackPressed();
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+    public void replaceFragment(@NonNull Fragment fragment) {
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+        count++;
+
+        updateShowHomeButton();
+    }
+
+    public void updateShowHomeButton() {
+        if (getSupportActionBar() != null) {
+            boolean enabled = count > 1;
+            getSupportActionBar().setDisplayHomeAsUpEnabled(enabled);
+            getSupportActionBar().setDisplayShowHomeEnabled(enabled);
         }
     }
 }
--- app/src/main/java/com/rakuishi/postalcode/view/fragment/PostalCodeListFragment.java
+++ app/src/main/java/com/rakuishi/postalcode/view/fragment/PostalCodeListFragment.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,28 +14,63 @@ import com.rakuishi.postalcode.R;
 import com.rakuishi.postalcode.databinding.FragmentPostalCodeListBinding;
 import com.rakuishi.postalcode.model.PostalCode;
 import com.rakuishi.postalcode.repository.PostalCodeRepository;
+import com.rakuishi.postalcode.view.activity.MainActivity;
 import com.rakuishi.postalcode.view.adapter.PostalCodeListAdapter;
+
+import java.util.List;
 
 import javax.inject.Inject;
 
+import io.reactivex.Single;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
 import timber.log.Timber;
 
+import static com.rakuishi.postalcode.view.fragment.PostalCodeListFragment.Type.CITY;
+import static com.rakuishi.postalcode.view.fragment.PostalCodeListFragment.Type.STREET;
+
 public class PostalCodeListFragment extends BaseFragment implements PostalCodeListAdapter.Callback {
 
+    public enum Type {
+        PREFECTURE, CITY, STREET;
+    }
+
+    private final static String TYPE = "type";
+    private final static String ID = "id";
     private FragmentPostalCodeListBinding binding;
     private PostalCodeListAdapter adapter;
+    private Type type;
+    private int id;
     @Inject
     PostalCodeRepository postalCodeRepository;
     @Inject
     CompositeDisposable compositeDisposable;
 
+    public static PostalCodeListFragment newInstance(Type type, int id) {
+        PostalCodeListFragment fragment = new PostalCodeListFragment();
+        Bundle args = new Bundle();
+        args.putSerializable(TYPE, type);
+        args.putInt(ID, id);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public static PostalCodeListFragment newInstance(Type type) {
+        return newInstance(type, 0);
+    }
+
+    public PostalCodeListFragment() {
+
+    }
+
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
         appComponent().inject(this);
+        type = (Type) getArguments().getSerializable(TYPE);
+        id = getArguments().getInt(ID);
+        Timber.d("type: " + type + ", id: " + id);
     }
 
     @Nullable
@@ -51,7 +87,21 @@ public class PostalCodeListFragment extends BaseFragment implements PostalCodeLi
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        Disposable disposable = postalCodeRepository.findPrefectures()
+        Single<List<PostalCode>> single;
+        switch (type) {
+            case PREFECTURE:
+                single = postalCodeRepository.findPrefectures();
+                break;
+            case CITY:
+                single = postalCodeRepository.findByPrefectureId(id);
+                break;
+            case STREET:
+            default:
+                single = postalCodeRepository.findByCityId(id);
+                break;
+        }
+
+        Disposable disposable = single
                 .subscribeOn(AndroidSchedulers.mainThread())
                 .subscribe((postalCodes, throwable) -> {
                     if (throwable == null) {
@@ -72,7 +122,23 @@ public class PostalCodeListFragment extends BaseFragment implements PostalCodeLi
 
     @Override
     public void onItemClick(PostalCode postalCode) {
-        // do something
+        Fragment fragment = null;
+        switch (type) {
+            case PREFECTURE:
+                fragment = newInstance(CITY, postalCode.prefectureId);
+                break;
+            case CITY:
+                // TODO: Support blank city pattern
+                fragment = newInstance(STREET, postalCode.cityId);
+                break;
+            case STREET:
+                // TODO: Add PostalCodeDetailFragment
+                break;
+        }
+
+        if (fragment != null && getActivity() != null && getActivity() instanceof MainActivity) {
+            ((MainActivity) getActivity()).replaceFragment(fragment);
+        }
     }
 
     // endregion
