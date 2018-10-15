e0014dbdf17ab736552e945015e37fad6fac7458
rakuishi <rakuishi@gmail.com>
2018-10-15 22:31:59 +0900

    Add privacy policy

--- app/src/main/java/com/rakuishi/postalcode/adapter/InfoAdapter.java
+++ app/src/main/java/com/rakuishi/postalcode/adapter/InfoAdapter.java
@@ -11,33 +11,40 @@ import android.widget.TextView;
 import com.rakuishi.postalcode.BuildConfig;
 import com.rakuishi.postalcode.R;
 
+import java.util.Locale;
+
 public class InfoAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     public interface Callback {
         void onClickUrl(String url);
     }
 
-    private final @NonNull Context context;
-    private final @NonNull Callback callback;
-    private final @NonNull LayoutInflater inflater;
+    private final @NonNull
+    Context context;
+    private final @NonNull
+    Callback callback;
+    private final @NonNull
+    LayoutInflater inflater;
 
-    public InfoAdapter(Context context, Callback callback) {
+    public InfoAdapter(@NonNull Context context, @NonNull Callback callback) {
         this.context = context;
         this.callback = callback;
         inflater = LayoutInflater.from(context);
     }
 
+    @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         return new ViewHolder(inflater.inflate(R.layout.item_two_line, parent, false));
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
         ViewHolder holder1 = (ViewHolder) holder;
         switch (position) {
             case 0:
-                holder1.primaryTextView.setText(BuildConfig.VERSION_NAME + "(" + BuildConfig.VERSION_CODE + ")");
+                String text = String.format(Locale.US, "%s(%d)", BuildConfig.VERSION_NAME, BuildConfig.VERSION_CODE);
+                holder1.primaryTextView.setText(text);
                 holder1.secondaryTextView.setText(R.string.version);
                 holder1.secondaryTextView.setVisibility(View.VISIBLE);
                 break;
@@ -50,23 +57,24 @@ public class InfoAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
                 holder1.primaryTextView.setText(R.string.developer);
                 holder1.secondaryTextView.setText(R.string.developed_by);
                 holder1.secondaryTextView.setVisibility(View.VISIBLE);
-                holder1.itemView.setOnClickListener(v -> {
-                    callback.onClickUrl(context.getString(R.string.developer_homepage));
-                });
+                holder1.itemView.setOnClickListener(v -> callback.onClickUrl(context.getString(R.string.developer_homepage)));
                 break;
             case 3:
                 holder1.primaryTextView.setText(R.string.license);
                 holder1.secondaryTextView.setVisibility(View.GONE);
-                holder1.itemView.setOnClickListener(v -> {
-                    callback.onClickUrl(context.getString(R.string.licenses_path));
-                });
+                holder1.itemView.setOnClickListener(v -> callback.onClickUrl(context.getString(R.string.licenses_path)));
+                break;
+            case 4:
+                holder1.primaryTextView.setText(R.string.privacy_policy);
+                holder1.secondaryTextView.setVisibility(View.GONE);
+                holder1.itemView.setOnClickListener(v -> callback.onClickUrl(context.getString(R.string.privacy_policy_page)));
                 break;
         }
     }
 
     @Override
     public int getItemCount() {
-        return 4;
+        return 5;
     }
 
     class ViewHolder extends RecyclerView.ViewHolder {
@@ -74,10 +82,10 @@ public class InfoAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
         TextView primaryTextView;
         TextView secondaryTextView;
 
-        public ViewHolder(View itemView) {
+        ViewHolder(View itemView) {
             super(itemView);
-            primaryTextView = (TextView) itemView.findViewById(R.id.primary_text_view);
-            secondaryTextView = (TextView) itemView.findViewById(R.id.secondary_text_view);
+            primaryTextView = itemView.findViewById(R.id.primary_text_view);
+            secondaryTextView = itemView.findViewById(R.id.secondary_text_view);
         }
     }
 }
